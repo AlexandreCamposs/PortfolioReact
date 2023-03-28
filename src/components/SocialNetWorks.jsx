efcba5052fd9922ceba213f252ccbd677f7b0495
AleCampos <alemdcamposs@gmail.com>
2023-03-28 20:07:35 +0044

    att

--- src/components/SocialNetWorks.jsx
+++ src/components/SocialNetWorks.jsx
@@ -2,15 +2,15 @@ import '../styles/components/socialNetWorks.sass'
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
 
 const socialNetWorks =[
-  {name:"linkedin", icon:<FaLinkedinIn/>},
-  {name:"github", icon:<FaGithub/>},
+  {name:"linkedin", icon:<FaLinkedinIn/>, URL:"https://www.linkedin.com/in/alexandre-moura-de-campos-496627114/"},
+  {name:"github", icon:<FaGithub/>, URL:"https://github.com/AlexandreCamposs"},
 ]
 
 function SocialNetWorks () {
   return(
     <section id="social-networks">
       {socialNetWorks.map((network)=>(
-        <a href="#" className='social-btn' id={network.name} key={network.name}>
+        <a href={network.URL} className='social-btn' id={network.name} key={network.name}>
           {network.icon}
         </a>
       ))}
